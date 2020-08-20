3471444ffce77e87ec6f8f6c9dba25d0a791bbe9
mrmujeeb <66780410+mrmujeeb@users.noreply.github.com>
2020-08-20 08:05:00 +0530

    today

--- src/gitomed/Test2.java
+++ src/gitomed/Test2.java
@@ -1,17 +1,27 @@
 package gitomed;
 
- class	FibonacciExample1{  
-		public static void main(String args[])  
-		{    
-		 int n1=0,n2=1,n3,i,count=10;    
-		 System.out.print(n1+" "+n2);//printing 0 and 1    
-		    
-		 for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
-		 {    
-		  n3=n1+n2;    
-		  System.out.print(" "+n3);    
-		  n1=n2;    
-		  n2=n3;    
-		 }    
-		  
-		}}  
+class CopyArray {    
+    public static void main(String[] args) {        
+             //Initialize array     
+        int [] arr1 = new int [] {1, 2, 3, 4, 5};     
+         //Create another array arr2 with size of arr1    
+        int arr2[] = new int[arr1.length];    
+        //Copying all elements of one array into another    
+        for (int i = 0; i < arr1.length; i++) {     
+            arr2[i] = arr1[i];     
+        }      
+         //Displaying elements of array arr1     
+        System.out.println("Elements of original array: ");    
+        for (int i = 0; i < arr1.length; i++) {     
+           System.out.print(arr1[i] + " ");    
+        }     
+            
+        System.out.println();    
+            
+        //Displaying elements of array arr2     
+        System.out.println("Elements of new array: ");    
+        for (int i = 0; i < arr2.length; i++) {     
+           System.out.print(arr2[i] + " ");    
+        }     
+    }    
+}
